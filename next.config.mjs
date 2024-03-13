055fa6fbfd86a9060636850fcc2a2cd69c5ff0a4
drivepc <driveasano@gmail.com>
2024-03-13 12:00:20 +0900

    sub directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV === "production";
+// console.log(process.env.NODE_ENV);
+
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/mosunset";
+const prefixPath = isProd ? SUB_DIRECTORY : "";
+
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
