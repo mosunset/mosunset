4d7265d56528117c7c6981cf132a0b044280cff8
drivepc <driveasano@gmail.com>
2024-03-21 21:35:53 +0900

    rock paper scissors done

--- src/app/game/Rock-Paper-Scissors/page.tsx
+++ src/app/game/Rock-Paper-Scissors/page.tsx
@@ -8,68 +8,100 @@ import Image from "next/image";
 import ibp from "@/components/imageBasePath";
 
 const Page = () => {
-    const [tabValue, settabValue] = useState("random");
-    const [cpuValue, setcpuValue] = useState("rock");
-    const [userValue, setuserValue] = useState("rock");
+    const [tabValue, setTabValue] = useState("random");
+    const [cpuValue, setCpuValue] = useState("rock");
+    const [userValue, setUserValue] = useState("rock");
     const [result, setResult] = useState("");
     const choices = ["rock", "scissors", "paper"];
 
-    /** min 以上 max未満 の整数を返す
-     *
-     * @param min
-     * @param max
-     * @returns
-     */
     const getRandomInt = (min: number, max: number) => {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
     };
+
     const onValueChange = (value: string) => {
-        settabValue(value);
+        setTabValue(value);
     };
+
+    const getResult = (cpuChoice: string, userChoice: string) => {
+        const cpuIndex = choices.indexOf(cpuChoice);
+        const userIndex = choices.indexOf(userChoice);
+        const diff = (cpuIndex - userIndex + 3) % 3;
+
+        if (diff === 0) {
+            return "あいこ🙂";
+        } else if (diff === 1) {
+            return "あなたの勝ち👍";
+        } else if (diff === 2) {
+            return "あなたの負け😅";
+        }
+    };
+
     const Result = (choice: string) => {
-        // TODO: 未完成
-        setuserValue(choice);
+        setUserValue(choice);
+
         switch (tabValue) {
             case "random":
-                setcpuValue(choices[getRandomInt(0, 3)]);
-                // setcpuValue("rock")
-                const diff = (choices.indexOf(cpuValue) - choices.indexOf(userValue) + 3) % 3;
-
-                if (diff === 0) {
-                    setResult("Draw");
-                } else if (diff === 1) {
-                    setResult("You Win");
-                } else {
-                    setResult("You Lose");
-                }
+                const randomChoice = choices[getRandomInt(0, 3)];
+                setCpuValue(randomChoice);
+                setResult(getResult(randomChoice, choice) as string);
                 break;
             case "win":
-                setcpuValue(choices[(choices.indexOf(choice) + 1) % 3]);
-                setResult("You Win");
+                const winChoice = choices[(choices.indexOf(choice) + 1) % 3];
+                setCpuValue(winChoice);
+                setResult("あなたの勝ち👍");
                 break;
             case "lose":
-                setcpuValue(choices[(choices.indexOf(choice) + 2) % 3]);
-                setResult("You Lose");
+                const loseChoice = choices[(choices.indexOf(choice) + 2) % 3];
+                setCpuValue(loseChoice);
+                setResult("あなたの負け😅");
                 break;
             default:
                 break;
         }
     };
+
     const ResultHtml = () => {
+        const userChoiceImage = ibp(`game/Rock-Paper-Scissors/${userValue}.webp`);
+        const cpuChoiceImage = ibp(`game/Rock-Paper-Scissors/${cpuValue}.webp`);
+
         return (
             <>
-                {tabValue}
-                <br />
-                cpu {cpuValue}
-                <br />
-                user {userValue}
-                <br />
-                {result}
+                <div className="mt-8 grid gap-4">
+                    <div className="flex items-center justify-center gap-4">
+                        <div className=" grid items-center justify-items-center">
+                            <span className="text-2xl font-bold">CPU</span>
+                            <div className="rounded-full bg-gray-200 p-4">
+                                <Image
+                                    src={cpuChoiceImage}
+                                    alt={cpuValue}
+                                    width={80}
+                                    height={80}
+                                    className="block aspect-square object-cover"
+                                />
+                            </div>
+                        </div>
+
+                        <div className=" grid items-center justify-items-center">
+                            <span className="text-2xl font-bold">You</span>
+                            <div className="rounded-full bg-gray-200 p-4 ">
+                                <Image
+                                    src={userChoiceImage}
+                                    alt={userValue}
+                                    width={80}
+                                    height={80}
+                                    className="block aspect-square object-cover"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-4xl font-bold text-center">{result}</div>
+                </div>
             </>
         );
     };
+
     const handChoice = () => {
         return (
             <>
@@ -78,7 +110,9 @@ const Page = () => {
                         <Button
                             key={choice}
                             variant="outline"
-                            className={"grid aspect-square h-auto justify-items-center p-2"}
+                            className={`grid aspect-square h-auto justify-items-center p-2 hover:bg-blue-300 ${
+                                choice === userValue ? "bg-blue-500 text-white" : ""
+                            }`}
                             onClick={() => Result(choice)}
                         >
                             <Image
@@ -107,28 +141,25 @@ const Page = () => {
                 >
                     <TabsList className="">
                         <TabsTrigger value="random" className="w-[100px]">
-                            ランダム
+                            通常
                         </TabsTrigger>
                         <TabsTrigger value="win" className="w-[100px]">
-                            勝ち
+                            必ず勝ち
                         </TabsTrigger>
                         <TabsTrigger value="lose" className="w-[100px]">
-                            負け
+                            必ず負け
                         </TabsTrigger>
                     </TabsList>
                     <TabsContent value="random">
                         {handChoice()}
-                        <br />
                         {ResultHtml()}
                     </TabsContent>
                     <TabsContent value="win">
                         {handChoice()}
-                        <br />
                         {ResultHtml()}
                     </TabsContent>
                     <TabsContent value="lose">
                         {handChoice()}
-                        <br />
                         {ResultHtml()}
                     </TabsContent>
                 </Tabs>
